2b9e63c24688e34ffcfa9bbc776b3b265136deef
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-02-22 03:40:58 +0200

    Update login.inc.php

--- LoginSystem/includes/login.inc.php
+++ LoginSystem/includes/login.inc.php
@@ -5,6 +5,34 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $pwd = $_POST['pwd'];
 
     try {
+
+        require_once 'dbh.inc.php';
+        require_once 'login_model.inc.php';
+        require_once 'login_contr.inc.php';
+
+        $errors = [];
+        //error handling
+        if(is_input_empty($username, $pwd, $email)){
+            $errors['empty_input'] = 'Fill in all fields!';
+        }
+        if(is_email_invalid($email)){
+            $errors['invalid_email'] = 'Invalid email used!';
+        }
+        if(is_username_taken($pdo, $username)){
+            $errors['username_taken'] = 'Username already taken!';
+        }
+        if(is_email_registered($pdo, $email)){
+            $errors['email_used'] = 'Email already registered!';
+        }
+
+        require_once 'config_session.inc.php';
+
+        if($errors){ //return false if there is no data in the array
+            $_SESSION['errors_signup'] = $errors;
+            header('Location: ../index.php');
+            die();
+        }
+
        
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
