f06d7c4b41a3561008be8c878fe4e71bf31eb009
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-02-28 08:27:47 +0200

    Decode Array

--- ToDoApp/newtodo.php
+++ ToDoApp/newtodo.php
@@ -3,10 +3,28 @@ echo '<pre>';
 echo var_dump($_POST);
 echo '/<pre>';
 
-$todo_name = $_POST['todo_name'] ?? false;
+$todoName = $_POST['todo_name'] ?? false;
+$todoName = trim($todoName);
 
-if($todo_name){
-    echo "set";
+if($todoName){
+
+    //Get the JSON file contents
+    $json = file_get_contents('todo.json');
+
+    //echo $json;
+
+    //Decode the JSON file into an associative PHP array
+    $jsonArray = json_decode($json, true);
+    //Add a new todo in the array
+    $jsonArray[$todoName] = ['completed' => 'false'];
+
+    
+    //var_dump($json_decoded);
+    //echo $json_decoded['Todo 1']["completed"];
+
+    //save the JSONarray back in to the JSON file
+    file_put_contents("todo.json", json_encode($jsonArray, JSON_PRETTY_PRINT));
+
 } else {
     echo "No new to do set";
 }
